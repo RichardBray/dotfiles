714a42dae90d6588c0520dcfd36197bbc1c671db
RichardBray <richardbray89@gmail.com>
2025-07-21 16:06:02 +0100

    feat: add vps_setup.sh

--- config/sketchybar/plugins/clock.sh
+++ config/sketchybar/plugins/clock.sh
@@ -3,10 +3,25 @@
 source "$CONFIG_DIR/colors.sh"
 
 # Get current date and time with day of week
-DATE=$(date '+%a %b %d')
+DAY_OF_WEEK=$(date '+%a')
+MONTH=$(date '+%b')
+DAY=$(date '+%d')
 TIME=$(date '+%H:%M')
 
-# Update the clock item
-sketchybar --set "$NAME" icon="" \
-                          label="$DATE $TIME"
+# Update the clock items - date in bold, time in regular
+if [[ "$NAME" == "clock.date" ]]; then
+    # Date portion with bold font
+    sketchybar --set "$NAME" label="$DAY_OF_WEEK $MONTH $DAY" \
+                              label.font="SF Pro:Bold:15.0"
+elif [[ "$NAME" == "clock.time" ]]; then
+    # Time portion with regular font
+    sketchybar --set "$NAME" icon="" \
+                              label="$TIME" \
+                              label.font="SF Pro:Regular:15.0"
+else
+    # Fallback for original single item
+    sketchybar --set "$NAME" icon="" \
+                              label="$DAY_OF_WEEK $MONTH $DAY $TIME" \
+                              label.font="SF Pro:Bold:15.0"
+fi
 
